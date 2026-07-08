daea831105bca5278f7473ad536479a6270acf1e
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-08 16:29:47 +1000

    Создал файл upload_file.py.

---
+++ sync_utilities/upload_file.py
@@ -0,0 +1,16 @@
+import os
+
+
+def upload_file(connector, filepath: str, logger) -> None:
+    """Загружает новый файл в облачное хранилище.
+
+    Args:
+        connector: Объект класса-коннектора к облачному хранилищу.
+        filepath: Путь к файлу на локальном компьютере.
+        logger: Настроенный объект логгера.
+    """
+    try:
+        connector.load(filepath)
+        logger.info(f"Загружен в облако: {os.path.basename(filepath)}")
+    except Exception as e:
+        logger.error(f"Ошибка загрузки {filepath}: {e}")
