afc069670581f6aae87a33e67a0c85398018ac89
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-12 00:49:01 +1000

    Удалил вывод ошибки в консоль, теперь ошибка пишется только в лог, logger получаем из setup_config.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import time
 
 from config_utilities.setup_config import setup_config
-from config_utilities.setup_logger import setup_logger
 from connecting_storage.yandex_disk_connecting import YandexDiskConnector
 from sync_utilities.sync_files import sync_files
 
@@ -15,14 +14,10 @@ def main() -> None:
     Завершается по нажатию Ctrl+C.
     """
     config_path = os.path.join(os.path.dirname(__file__), ".env")
-    config = setup_config(config_path)
-    logger = setup_logger(config["LOG_FILE_PATH"])
+    config, logger = setup_config(config_path)
 
     logger.info("Запуск приложения")
     logger.info(f"Синхронизируемая папка: {config['SYNC_FOLDER_PATH']}")
-    print("Запуск приложения")
-    print(f"Синхронизируемая папка: {config['SYNC_FOLDER_PATH']}")
-    print("-" * 100)
 
     connector = YandexDiskConnector(
         token=config["YANDEX_TOKEN"],
@@ -39,8 +34,6 @@ def main() -> None:
             sync_files(connector, config["SYNC_FOLDER_PATH"], logger)
     except KeyboardInterrupt:
         logger.info("Завершение работы приложения")
-        print("-" * 100)
-        print("Завершение работы приложения")
 
 
 if __name__ == "__main__":
